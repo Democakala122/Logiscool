4d62526a4925cdc8ced17b5d7df9918080f602b0
Democakala122 <domoca2011@gmail.com>
2025-02-18 18:03:59 +0100

    %-os kiiratás és ismétlés

---
+++ 3ora.py
@@ -0,0 +1,22 @@
+my_name = "Domó"
+print("Az én nevem" + my_name)
+print(f"Az én nevem {my_name}")
+
+my_age = 21
+print("Az én élétkorom: " + str(my_age))
+
+pet_name = "Morzsi"
+pet_age = 5
+pet_species = "kutya"
+print(f"A háziállatom neve: {pet_name}, {pet_age} éves és egy {pet_species}")
+
+pet_name = "Cirmi"
+pet_age = 2
+pet_species = "cica"
+print(f"A háziállatom neve: {pet_name}, {pet_age} éves és egy {pet_species}")
+
+#TO DO 
+name = input("Add meg a neved! ")
+age = input("Add meg az életkorod! ")
+print("A te neved: %s" %name)
+print("A te neved %s és %d éves vagy" %(name, age))
