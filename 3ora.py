5d77e09ff36dbb3dce5cf35d5f675889f5b3826e
Democakala122 <domoca2011@gmail.com>
2025-02-18 18:43:11 +0100

    önálló feladat

--- 3ora.py
+++ 3ora.py
@@ -20,3 +20,53 @@ name = input("Add meg a neved! ")
 age = input("Add meg az életkorod! ")
 print("A te neved: %s" %name)
 print("A te neved %s és %d éves vagy" %(name, age))
+
+
+num = 5
+word = "auto"
+is_boy = True
+is_animal = False
+
+if num:
+    print("Ez egy szám")
+if word:
+    print("Ez egy karakter")
+if is_boy:
+    print("Ő egy fiú")
+if is_animal:
+    print("Ő egy állat")
+else:
+    print("Ő nem egy állat")
+
+num1 = 12
+num2 = 33
+num3 = 66
+
+if num1 > num2:
+    print("Az első szám nagyobb mint a második")
+if num1 < num2 and num2< num3: # and, or, <, >
+    print("Kutya")
+else:
+    print(" A második szám nagyonbb mint az első")
+
+a1= int(input("Kérem az első számot: "))
+a2 = int(input("Kérem az első számot: "))
+
+if a1>9:
+    print("Ez a szám kétjegyű")
+elif a1>a2:
+    print("Az első szám nagyobb mint a második")
+
+#SAJÁT FELADAT
+
+number = int(input("Mi a pontszámod? "))
+if number > 90:
+    print("5-öst kaptál!")
+elif number > 75:
+    print("4-öst kaptál!")
+elif number > 60:
+    print("3-öst kaptál!")
+elif number > 40:
+    print("2-öst kaptál!")
+else:
+    print("1-öst kaptál!")
---
+++ 3ora_sajatfeladat.py
@@ -0,0 +1,12 @@
+number = int(input("Mi a pontszámod? "))
+
+if number > 90:
+    print("5-öst kaptál!")
+elif number > 75:
+    print("4-öst kaptál!")
+elif number > 60:
+    print("3-öst kaptál!")
+elif number > 40:
+    print("2-öst kaptál!")
+else:
+    print("1-öst kaptál!")
