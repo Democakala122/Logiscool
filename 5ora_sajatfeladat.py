d806b87be634254a1859c7c74d1cd9314ac5562d
Democakala122 <domoca2011@gmail.com>
2025-03-04 18:57:46 +0100

    5ora sajatfeladat megoldas

--- 5ora_sajatfeladat.py
+++ 5ora_sajatfeladat.py
@@ -19,6 +19,11 @@
 *
 """
 
+import math
+i = -7
+while i < 8:
+    print("*" * int( 8- math.fabs(i) ) )
+    i += 1
 
 # Olvassunk be folyamatosan számokat, amiket összegezünk, egész adig amíg 0-t nem írunk be
 
@@ -33,12 +38,26 @@ print(ossz)
 
 
 # Írjunk egy programot, ami eldönti egy számról, hogy prímszán-e! While ciklussal
+"""
 szam = int(input("Írj be egy számot amiről szeretnéd tudni, hogy prímszám-e: \n"))
 num = 2
-while num < szam-1 and szam % num != 0:
+while num < szam // 2 and szam % num != 0:
     num += 1
 
 if szam % num == 0:
     print("Ez nem egy prímszám")
 else:
     print("Ez egy prímszám")
+"""
+
+num = 10
+while True:
+    isPrime = True
+    i = 2
+    while isPrime and i <= num//2:
+        if num % i == 0:
+            isPrime = False
+        i += 1
+    if isPrime:
+        print(num)
+    num += 1
