3fe7240e4ea64a46abb197dd9b2cf7d0df1f25c7
Democakala122 <domoca2011@gmail.com>
2025-03-18 18:24:24 +0100

    Logikai, összehasonlító, értékadó operátorok

--- 7ora.py
+++ 7ora.py
@@ -31,3 +31,49 @@ print(2**(1/2)) # float
 # // maradék nélküli osztás
 # % osztás maradéka
 # ** hatványozás, gyökvonás törtekkel
+
+
+
+# Értékadó operátorok (szinte bármi amiben =-jel van)
+#egy változónak értéket ad
+num = 10 
+print(f"num = {num}") # 10
+num += 5 # A num értékéhez hozzá ad 5-öt
+print(f"num = {num}") # 15
+num -= 3
+print(f"num = {num}") #12
+num *= 4
+print(f"num = {num}") #48
+num /= 2
+print(f"num = {num}") #24.0 num FLOAT lesz
+num //= 2
+print(f"num = {num}") #12 ha num FLOAT akkor az marad, amúgy int
+num **= 2
+print(f"num = {num}") #144 ha num FLOAT akkor az marad, amúgy int
+
+
+
+#Összehasonlító operátorok (True vagy False értéket adnak)
+#Két értéket összehasonlít
+print(5 > 3) # True
+print(5 < 2) # False
+print(5 == 8) # False
+print(5 >= 5) # True
+print(4 <= 2) # False
+print(4 != 2) #True
+
+
+#Logikai operátorok
+#Logikai értékekkel kezdenek valamit
+# (ÉS VAGY, NEM)
+#Logikai érték eredményük (True, False)
+print(5 > 3 and 3 > 4) # True and False -> False
+print(5 > 1 or 3 > 4)# True and False -> True
+print(not 5 > 1)
+
+#Logikai kapuk:
+# A | B | A and B | A vagy B | nem A | nem B 
+# i | i |    i    |     i    |   h   |   h
+# i | h |    h    |     i    |   h   |   i
+# h | i |    h    |     i    |   i   |   h
+# h | h |    h    |     h    |   i   |   i
